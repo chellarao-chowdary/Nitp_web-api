d4a7a765bc557c0728257c465fb5cf1f2b69c9a3
chellarao chowdary <chowdarylucky123@gmail.com>
2020-05-28 10:23:12 +0530

    adding files

---
+++ scrape.py
@@ -0,0 +1,70 @@
+import requests,re
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+import time
+
+
+def makingsoup():
+	try:
+		res=requests.get("http://www.nitp.ac.in/php/home.php")
+	except: # If website is down or can't reach... it sleeps for 3 minutes and try's recursively
+		time.sleep(300)
+		makingsoup()
+	return(BeautifulSoup(res.text,'html.parser'))
+	
+
+# Announcement Section Tab
+def announcements():
+	soup=makingsoup()
+	_div = soup.find('div', {'class':'ex1'})
+	z= _div.find_all('a')
+	return collection(z)
+	
+# It creates a list and add the title and links to the list
+def collection(para):
+	cool=[]
+	for i in para:
+		title= i.text
+		link= i.get('href')
+		if link[0]!='h':
+			link='http://www.nitp.ac.in'+quote(link[2:])
+		cool.append({'title':title,'link':link})
+	cool.reverse()
+	return cool
+
+# Important Section Tab
+def important():
+	soup=makingsoup()
+	_div1=soup.find('div',{'class':'ex2'})
+	y=_div1.find_all('a')
+	return collection(y)
+
+
+
+
+
+def main():
+	notice = announcements()
+	notice1 = important()
+
+
+	import pickle
+
+	with open('ann_list', 'wb') as fp:
+		pickle.dump(notice, fp)
+
+
+	with open('imp_list','wb') as fp:
+		pickle.dump(notice1,fp)
+
+	with open ('ann_list', 'rb') as fp:
+		itemlist = pickle.load(fp)
+	print(itemlist,len(itemlist))
+
+	with open ('imp_list', 'rb') as fp:
+		print(pickle.load(fp))
+
+
+if __name__ == '__main__':
+	main()
+
