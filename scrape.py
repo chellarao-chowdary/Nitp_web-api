1dcce25c792c967b4f0d8b869c317459f62886b2
chowdary <57833491+chellarao-chowdary@users.noreply.github.com>
2020-05-29 10:40:31 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -10,7 +10,7 @@ def makingsoup():
 	except: # If website is down or can't reach... it sleeps for 3 minutes and try's recursively
 		time.sleep(300)
 		makingsoup()
-	return(BeautifulSoup(res.text,'html.parser'))
+	return(BeautifulSoup(res.text,'lxml'))
 	
 
 # Announcement Section Tab
@@ -40,28 +40,43 @@ def important():
 	return collection(y)
 
 
+def events():
+	soup=makingsoup()
+	_div2=soup.select('#opi')
+	x=_div2[0].find_all('a')
+	return collection(x)
 
 
 
 def main():
 	notice = announcements()
 	notice1 = important()
+	notice2=events()
 
 
 	import pickle
-
+	
+# Dumping into the pickle file
 	with open('ann_list', 'wb') as fp:
 		pickle.dump(notice, fp)
 
 
 	with open('imp_list','wb') as fp:
 		pickle.dump(notice1,fp)
+		
+	with open('notice_list','wb') as fp:
+		pickle.dump(notice2,fp)
+
+# To reuse the stored data from a pickle file
 
 	with open ('ann_list', 'rb') as fp:
 		itemlist = pickle.load(fp)
 	print(itemlist,len(itemlist))
 
 	with open ('imp_list', 'rb') as fp:
+		print(pickle.load(fp))
+		
+	with open ('notice_list','rb') as fp:
 		print(pickle.load(fp))
 
 
